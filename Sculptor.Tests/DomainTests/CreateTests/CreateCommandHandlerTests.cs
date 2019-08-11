bfd78f33563f3adfec8d660f24d8493c95eb0994
MichaelSearson <10487416+MichaelSearson@users.noreply.github.com>
2019-08-11 19:17:44 +0100

    Uncomment unit tests and fix compatibility issues
    All previous unit tests have been uncommented and updated to match the
    changes made in the previous commit (`877e87d`). This includes updating
    the test project's folder structure to mirror that of `Sculptor.Core`
    so it's easy to find the tests related to a given component.

---
+++ Sculptor.Tests/DomainTests/CreateTests/CreateCommandHandlerTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO.Abstractions.TestingHelpers;
+using Moq;
+using NUnit.Framework;
+using Sculptor.Core;
+using Sculptor.Core.Domain.Create;
+using Sculptor.Infrastructure.ConsoleAbstractions;
+
+namespace Sculptor.Tests.DomainTests.CreateTests
+{
+    public class CreateCommandHandlerTests
+    {
+        private Mock<ITerminal> _mockTerminal;
+
+        #region Setup
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockTerminal = new Mock<ITerminal>();
+            _mockTerminal.Setup(x => x.RenderText(It.IsAny<string>()));
+        }
+
+        #endregion
+
+        [Test]
+        public void CreateCommandHandlerOnSuccessHasValidDirectoryStructure()
+        {
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddDirectory(Environment.CurrentDirectory);
+
+            var handler = new CreateCommandHandler(
+                _mockTerminal.Object,
+                mockFileSystem);
+
+            const string projectName = "MyUnitTestProject";
+            const string outputDirectoryName = "public";
+
+            handler.Handle(new CreateCommand
+            {
+                OutputDirectoryName = outputDirectoryName,
+                ProjectName = projectName
+            });
+
+            string projectRootPath = mockFileSystem.Path.Combine(
+                Environment.CurrentDirectory,
+                projectName);
+
+            string projectOutputPath = mockFileSystem.Path.Combine(
+                projectRootPath,
+                outputDirectoryName);
+
+            string projectContentPath = mockFileSystem.Path.Combine(
+                projectRootPath,
+                ReservedDirectories.ContentDirectoryName);
+
+            Assert.Multiple(() =>
+            {
+                Assert.True(mockFileSystem.Directory.Exists(projectRootPath));
+                Assert.True(mockFileSystem.Directory.Exists(projectOutputPath));
+                Assert.True(mockFileSystem.Directory.Exists(projectContentPath));
+            });
+        }
+
+        [Test]
+        public void CreateCommandHandlerOnSucessHasValidSettingsFile()
+        {
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddDirectory(Environment.CurrentDirectory);
+
+            var handler = new CreateCommandHandler(
+                _mockTerminal.Object,
+                mockFileSystem);
+
+            const string projectName = "MyUnitTestProject";
+
+            handler.Handle(new CreateCommand
+            {
+                OutputDirectoryName = "public",
+                ProjectName = projectName
+            });
+
+            string projectRootPath = mockFileSystem.Path.Combine(
+                Environment.CurrentDirectory,
+                projectName);
+
+            Assert.True(mockFileSystem.File.Exists(mockFileSystem.Path.Combine(
+                projectRootPath,
+                "appsettings.json")));
+        }
+
+        [Test]
+        public void CreateCommandHandlerOnSuccessInformsUserOfActionsPerformed()
+        {
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddDirectory(Environment.CurrentDirectory);
+
+            var handler = new CreateCommandHandler(
+                _mockTerminal.Object,
+                mockFileSystem);
+
+            handler.Handle(new CreateCommand
+            {
+                OutputDirectoryName = "public",
+                ProjectName = "MyUnitTestProject"
+            });
+
+            _mockTerminal.Verify(mock => mock.RenderText(It.IsAny<string>()), Times.Exactly(3));
+        }
+    }
+}
---
+++ Sculptor.Tests/DomainTests/CreateTests/CreateCommandValidatorTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Sculptor.Core;
+using Sculptor.Core.Domain.Create;
+using Sculptor.Infrastructure.ConsoleAbstractions;
+
+namespace Sculptor.Tests.DomainTests.CreateTests
+{
+    public class CreateCommandValidatorTests
+    {
+        private Mock<ITerminal> _mockTerminal;
+
+        #region Setup
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockTerminal = new Mock<ITerminal>();
+            _mockTerminal.Setup(x => x.RenderText(It.IsAny<string>()));
+        }
+
+        #endregion
+
+        [Test]
+        public void CreateCommandValidatorFailsIfOutputDirectoryMatchesReservedName()
+        {
+            var mockReservedDirectories = new Mock<IReservedDirectories>();
+            mockReservedDirectories
+                .Setup(x => x.IsDirectoryNameReserved(It.IsAny<string>()))
+                .Returns(true);
+
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddDirectory(Environment.CurrentDirectory);
+
+            var command = new CreateCommand
+            {
+                ProjectName = "MyUnitTestProject",
+                // It doesn't matter what this is set to as we have stubbed the
+                // `IsDirectoryNameReserved` method above to always return true.
+                OutputDirectoryName = "reserved-for-internal-use"
+            };
+
+            var validator = new CreateCommandValidator(
+                mockFileSystem,
+                mockReservedDirectories.Object);
+
+            var validationResult = validator.Validate(command);
+
+            const string expectedErrorMessage = "The value provided for: `output` is reserved for internal use by Sculptor";
+
+            bool hasExpectedNumberOfErrors = validationResult.Errors.Count() == 1;
+            bool hasExpectedErrorMessage = validationResult
+                .Errors
+                .Any(x => x.ErrorMessage == expectedErrorMessage);
+
+            Assert.Multiple(() =>
+            {
+                Assert.True(hasExpectedNumberOfErrors);
+                Assert.True(hasExpectedErrorMessage);
+            });
+        }
+
+        [TestCase("\0", "public", 1)]
+        [TestCase("MyUnitTestProject", "\0", 1)]
+        [TestCase("\0", "\0", 2)]
+        public void CreateCommandValidatorFailsForInvalidCommand(
+            string projectName,
+            string outputDirectoryName,
+            int expectedNumberOfErrors)
+        {
+            var mockReservedDirectories = new Mock<IReservedDirectories>();
+            mockReservedDirectories
+                .Setup(x => x.IsDirectoryNameReserved(It.IsAny<string>()))
+                .Returns(true);
+
+            var mockFileSystem = new MockFileSystem();
+
+            var command = new CreateCommand
+            {
+                ProjectName = projectName,
+                OutputDirectoryName = outputDirectoryName
+            };
+
+            var validator = new CreateCommandValidator(
+                mockFileSystem,
+                mockReservedDirectories.Object);
+
+            var validationResult = validator.Validate(command);
+
+            const string expectedErrorMessage = "Invalid path character(s) provided for:";
+            bool hasExpectedNumberOfErrors = validationResult.Errors.Count() == expectedNumberOfErrors;
+            bool hasExpectedErrorMessage = validationResult
+                .Errors
+                .Any(x => x.ErrorMessage.StartsWith(
+                    expectedErrorMessage,
+                    StringComparison.Ordinal));
+
+            Assert.Multiple(() =>
+            {
+                Assert.True(hasExpectedNumberOfErrors);
+                Assert.True(hasExpectedErrorMessage);
+            });
+        }
+
+        [Test]
+        public void CreateCommandValidatorFailsIfProjectAlreadyExists()
+        {
+            var mockReservedDirectories = new Mock<IReservedDirectories>();
+
+            var mockFileSystem = new MockFileSystem();
+
+            const string projectName = "MyUnitTestProject";
+            const string outputDirectoryName = "public";
+
+            string projectRootPath = mockFileSystem.Path.Combine(
+                Environment.CurrentDirectory,
+                projectName);
+
+            // Simulate the `Create` command already having been successfully run by
+            // adding a directory with the same project name.
+            mockFileSystem.AddDirectory(projectRootPath);
+
+            var command = new CreateCommand
+            {
+                ProjectName = projectName,
+                OutputDirectoryName = outputDirectoryName
+            };
+
+            var validator = new CreateCommandValidator(
+                mockFileSystem,
+                mockReservedDirectories.Object);
+
+            var validationResult = validator.Validate(command);
+
+            string expectedErrorMessage = $"A Sculptor project with the name `{projectName}` already exists in the provided directory";
+            bool hasExpectedNumberOfErrors = validationResult.Errors.Count() == 1;
+            bool hasExpectedErrorMessage = validationResult
+                .Errors
+                .Any(x => x.ErrorMessage == expectedErrorMessage);
+
+            Assert.Multiple(() =>
+            {
+                Assert.True(hasExpectedNumberOfErrors);
+                Assert.True(hasExpectedErrorMessage);
+            });
+        }
+    }
+}
